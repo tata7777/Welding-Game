ce3b0d3229e3c576d08f870939425a69d8af7914
Tássio <90060457+tata7777@users.noreply.github.com>
2022-03-09 16:00:48 -0300

    Update conexaobd.php

--- QUIZ funcionando/conexaobd.php
+++ QUIZ funcionando/conexaobd.php
@@ -22,7 +22,7 @@ function conectabd()
     $error = (file_exists('scores.sqlite')) ? "Impossible to open, check permissions" : "Impossible to create, check permissions";
     die($error);
   }
-  $db->query("CREATE TABLE IF NOT EXISTS scores (name varchar(80), university varchar(80), country varchar(80), score integer)"); //colocar language tambem
+  $db->query("CREATE TABLE IF NOT EXISTS scores (name varchar(80), university varchar(80), country varchar(80), score integer)"); //colocar language e data tambem e o score deve começar com 0
   return $db;
 }
 
